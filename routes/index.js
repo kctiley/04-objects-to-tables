120978e26082a7fb16e1f222b68f148516a4d9d8
Klint Tiley <kctiley@gmail.com>
2015-09-01 16:49:30 -0600

    Refactored for cleaner code.

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,13 @@ var express = require('express');
 var router = express.Router();
 var peopleArr = require('../lib/data.js');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-router.get('/showtable', function(req, res, next){
+router.get('/show_table', function(req, res, next){
   res.render('show_table', {title: 'Listing All People', data: peopleArr.all})
 });
-
-router.get('/number', function(req, res, next){
-  res.render('show_number', { title: 'Phone Number', phone: data[i].telephone_number})
-});
-
-
 
 module.exports = router;
