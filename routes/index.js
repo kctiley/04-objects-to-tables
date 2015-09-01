9cbdd3534e4371bebbeb4dd1ded5b41c6ca11b6e
Klint Tiley <kctiley@gmail.com>
2015-09-01 15:49:29 -0600

    Table showing with ejs

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/showtable', function(req, res, next){
+  res.render('show_table', {title: 'Table Page', data: peopleArr.all})
+});
+
+
+
+
+
+//different show views with iterating in js
+// var details = function(){
+//   var firstNamesToArr = [];
+//   for (var i = 0; i < peopleArr.all.length; i++){
+//     firstNamesToArr.push(peopleArr.all[i].first_name + "<br>" )
+//   };
+//   return firstNamesToArr.join(' ');
+// }
+// router.get('/show2', function(req, res, next) {
+// res.send(details())
+// });
+
 router.get('/show', function(req, res, next) {
   res.render('index', { title: 'Express', showList: peopleArr.all[0].first_name });
 });
+
+
+// router.get('/show3', function(req, res, next) {
+//   res.render('index', { title: 'Express', showList: res.send(details())});
+// });
+
+
+
 
 module.exports = router;
